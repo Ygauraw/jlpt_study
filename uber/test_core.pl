b45d3acb60a28dd797c1c00275fcfb7fa0b32dc3
Declan Malone <idablack@users.sourceforge.net>
2016-07-31 01:41:04 +0100

    Starting program to test Core 2k/6k vocabulary.

---
+++ uber/test_core.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+
+# Use a bunch of different libraries to give a test on Core 6k (or 2k)
+# vocabulary.
+#
+# Class::DBI to encapsulate database objects
+# Gtk2::Ex::FormFactory to do most GUI stuff
+# Gtk2::WebKit for <audio> support
+# Net::OnlineCode::RNG for replayable random selections
+#
+#
+# 
+
+use strict;
+use warnings;
+use utf8;
+
+binmode STDOUT, ":utf8";
+binmode STDIN,  ":utf8";
+
+use Data::Dump qw(dump dumpf pp);
+
+require 'test_core_model.pm';
+
+# test out Class::DBI stuff
+my $core2k = CoreVocab::Core2k->retrieve(1);
+
+print "First Core2k record:\n" ;
+print "  ID: "           . $core2k->id  . "\n";
+print "  JA_vocab: "     . $core2k->ja_vocab  . "\n";
+print "  JA_kana: "      . $core2k->ja_vocab_kana  . "\n";
+print "  EN_vocab: "     . $core2k->en_vocab  . "\n";
+
+# How do I get linked records (ie, has_a relationships)?
+# Core2k has a has_a Vocab and Sentence object linked to it
+
+# pp provides some interesting information at this point
+#pp $core2k;
+
+print "  Vocab JA_text: "        . $core2k->vocab_id->ja_text  . "\n";
+print "  Vocab POS: "            . $core2k->vocab_id->pos  . "\n";
+print "  Vocab 2k sequence: "    . $core2k->vocab_id->core_2k_seq  . "\n";
+print "  Vocab Vocab Sound ID: " . $core2k->vocab_id->sound_id  . "\n";
+print "  Sentence JA: "          . $core2k->main_sentence_id->ja_text  . "\n";
+
+# OK, that tests basic retrieval and retrieval from foreign tables
+
+# The one thing I wasn't sure about in the schema is the relationship
+# between the Core6k table and Core6kSentence
+
+my $core6k = CoreVocab::Core6k->retrieve(2);
+
+print "Second Core6k record:\n" ;
+print "  ID: "           . $core6k->id  . "\n";
+print "  JA_vocab: "     . $core6k->ja_vocab  . "\n";
+print "  JA_kana: "      . $core6k->ja_vocab_kana  . "\n";
+print "  EN_vocab: "     . $core6k->en_vocab  . "\n";
+
+#pp $core6k;
+# The following prints how many sentences there are. Thanks to the
+# magic of auto-stringification, I guess
+print "  Sentences: "    . $core6k->sentences  . "\n";
+
+foreach my $sen ($core6k->sentences) {
+    print "[" . $sen->core_6k_id . ":" .$sen->sentence_id . "] " .
+	$sen->sentence_id->ja_text . "\n";
+}
---
+++ uber/test_core_model.pm
@@ -0,0 +1,103 @@
+#
+# Class::DBI stuff for accessing Core database
+
+package CoreVocab::DBI;
+use base 'Class::DBI::SQLite';
+
+# based partly on tutorial code that comes with FormFactory
+
+# Should probably make DB connection string an option somewhere
+# instead of hard-wiring it.
+
+CoreVocab::DBI->connection(
+    "dbi:SQLite:dbname=/home/dec/JLPT_Study/core_6000/core_2k_6k.sqlite",'','',
+    {   # does Class::DBI accept DBI/DBD options like below? Yes.
+        RaiseError     => 1,
+        sqlite_unicode => 1,
+        AutoCommit     => 0,
+    }
+    );
+
+#sub accessor_name_for { "get_$_[1]" }
+#sub mutator_name_for  { "set_$_[1]" }
+#sub autoupdate        { 1 }
+
+# Whereas tables are set up with plural names (eg, "sounds"), objects
+# will have singular names.
+
+#################
+package CoreVocab::Sound;
+use base CoreVocab::DBI;
+
+CoreVocab::Sound->table('sounds');
+CoreVocab::Sound->columns(All => qw/id url type content_type local_filename audio/);
+
+#################
+package CoreVocab::Image;
+use base CoreVocab::DBI;
+
+CoreVocab::Image->table('images');
+CoreVocab::Image->columns(All => qw/id url type content_type local_filename audio/);
+
+#################
+package CoreVocab::Vocab;
+use base CoreVocab::DBI;
+
+CoreVocab::Vocab->table('vocabulary');
+CoreVocab::Vocab->columns(All => qw/id ja_text ja_kana ja_hira ja_romaji ja_alt
+    en_text en_text_2k en_text_6k pos core_2k_seq sound_id/);
+
+CoreVocab::Vocab->has_a(sound_id => 'CoreVocab::Sound');
+
+#################
+package CoreVocab::Sentence;
+use base CoreVocab::DBI;
+
+CoreVocab::Sentence->table('sentences');
+CoreVocab::Sentence->columns(All => qw/id ja_text  ja_kana ja_hira ja_romaji ja_ruby 
+    en_text en_text_2k en_text_6k sound_id image_id/);
+
+# We have sound and images
+CoreVocab::Sentence->has_a(sound_id => 'CoreVocab::Sound');
+CoreVocab::Sentence->has_a(image_id => 'CoreVocab::Image');
+
+#################
+package CoreVocab::Core2k;
+use base CoreVocab::DBI;
+
+CoreVocab::Core2k->table('core_2k');
+CoreVocab::Core2k->columns(All => qw/id ja_vocab ja_vocab_kana en_vocab vocab_id 
+    main_sentence_id/);
+
+# We have full vocab item (includes sound/image links) and a single sentence
+CoreVocab::Core2k->has_a(vocab_id => 'CoreVocab::Vocab');
+CoreVocab::Core2k->has_a(main_sentence_id => 'CoreVocab::Sentence');
+
+#################
+package CoreVocab::Core6kSentence;
+use base CoreVocab::DBI;
+CoreVocab::Core6kSentence->table('core_6k_sentences');
+
+# This table has a a composite primary key
+CoreVocab::Core6kSentence->columns(Primary => qw/core_6k_id sentence_id/);
+
+# And it has one sentence, indexed by sentence_id
+CoreVocab::Core6kSentence->has_a(sentence_id => 'CoreVocab::Sentence');
+
+# It seems this sets up the reciprocal link for the foreign key used
+# in Core6k. This class must come before that one.
+CoreVocab::Core6kSentence->has_a(core_6k_id =>'CoreVocab::Core6k');
+
+#################
+package CoreVocab::Core6k;
+use base CoreVocab::DBI;
+
+CoreVocab::Core6k->table('core_6k');
+CoreVocab::Core6k->columns(All => qw/id ja_vocab ja_vocab_kana en_vocab/);
+
+# For has_many to work, we need to have the reciprocal has_a link
+# set up prior to this
+CoreVocab::Core6k->has_many(sentences => 'CoreVocab::Core6kSentence',
+			    { order_by => 'sentence_id' });
+
+1;
