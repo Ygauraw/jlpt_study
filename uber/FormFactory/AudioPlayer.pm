fcf93129a4d4a71328632c173e205df9938df881
Declan Malone <idablack@users.sourceforge.net>
2016-08-02 20:33:52 +0100

    Tidying up some stuff, moving new widget into subdir.

--- uber/FormFactory/AudioPlayer.pm
+++ uber/FormFactory/AudioPlayer.pm
@@ -16,7 +16,10 @@ use warnings;
 # Class data: player script that will be the same for all instances
 our $player_script = <<'END';
 
-var wait_timer;
+var audio;
+var len;
+var trackno   = 0;
+
 function init() {
   audio     = document.getElementById("audio");
   trackno   = 0;
@@ -39,14 +42,22 @@ function init() {
   // Instead use addEventListener
   audio.addEventListener("ended", after);
 
-
   load_audio(audio, trackno);  
   if (auto_play) {
+    console.log("auto_play variable on, auto-playing");
+
+    // it appears that autoplay is needed on the <audio> element. Its
+    // purpose is to auto-play when the media file actually loads.
+    // Without the autoplay attribute, the following does nothing,
+    // presumably because the file hasn't been loaded yet. An
+    // alternative way to fix this would probably be to add an onload
+    // event trigger
     play();
   }
 }
 function after() {
   console.log ("Got to after()");
+  if (!auto_advance) { return };
   wait_timer = setTimeout(advance, delay);
 }
 function advance() {
@@ -71,18 +82,19 @@ function play() {
 function load_audio(player, trackno) {
   player.src = playlist[trackno];
 }
-
-
 END
 
 
-# accessors 
+# Accessors. These are for the Perl side, which only deals with
+# initial construction of the webkit + html + script bundle. To get
+# access to the state of the WebKit object, we need different sub.
 sub get_track_delay_ms          { shift->{track_delay_ms}               }
 sub get_uri_base                { shift->{uri_base}                     }
 sub get_auto_play               { shift->{auto_play}                    }
 sub get_playlist                { shift->{playlist}                     }
 sub get_loop                    { shift->{loop}                         }
-sub get_allow_file_uri          { shift->{playlist}                     }
+sub get_allow_file_uri          { shift->{allow_file_uri}               }
+sub get_auto_advance            { shift->{auto_advance}                 }
 
 sub set_track_delay_ms          { shift->{track_delay_ms}       = $_[1] }
 sub set_uri_base                { shift->{uri_base}             = $_[1] }
@@ -90,6 +102,7 @@ sub set_auto_play               { shift->{auto_play}            = $_[1] }
 sub set_playlist                { shift->{playlist}             = $_[1] }
 sub set_loop                    { shift->{loop}                 = $_[1] }
 sub set_allow_file_uri          { shift->{allow_file_uri}       = $_[1] }
+sub set_auto_advance            { shift->{auto_advance}         = $_[1] }
 
 # stash the gtk widget(s)
 sub get_gtk_vbox                { shift->{gtk_vbox}                     }
@@ -104,11 +117,12 @@ sub new {
     my $class = shift;
     my %o = (
         track_delay_ms => 600,	# default args
-	auto_play      => 1,
-	uri_base       => '',
 	playlist       => undef,
-	allow_file_uri => 1,
+	auto_play      => 1,
+	auto_advance   => 1,
 	loop           => 0,
+	uri_base       => '',
+	allow_file_uri => 0,
         @_,			# user args
 
 	width          => 400,	# parent class args
@@ -118,11 +132,14 @@ sub new {
     my $self = $class->SUPER::new(%o);
 
     # stash options (using hash slice to pull out option keys)
-    my ($track_delay_ms, $auto_play, $uri_base, $playlist, $allow_file_uri, $loop) =
-	@o{qw/track_delay_ms auto_play uri_base playlist allow_file_uri loop/};
+    my ($track_delay_ms, $auto_play, $auto_advance, $uri_base, $playlist, 
+	$allow_file_uri, $loop) =
+	@o{qw/track_delay_ms auto_play auto_advance uri_base playlist
+              allow_file_uri loop/};
 
     $self->set_track_delay_ms($track_delay_ms);
     $self->set_auto_play($auto_play);
+    $self->set_auto_advance($auto_advance);
     $self->set_uri_base($uri_base);
     $self->set_playlist($playlist);
     $self->set_loop($loop);
@@ -134,12 +151,14 @@ sub new {
 sub build_html {
     my $self = shift;
 
-    my $auto_play = $self->get_auto_play;
-    my $autoplay = $auto_play ? "autoplay" : ""; # HTML tag
-    my $delay = $self->get_track_delay_ms;
-    
-    my $loop = $self->get_loop;
+    my $auto_play    = $self->get_auto_play;
+    my $auto_advance = $self->get_auto_advance;
+    my $delay        = $self->get_track_delay_ms;
+    my $loop         = $self->get_loop;
 
+    # for setting the attribute to the HTML <audio> tag
+    my $autoplay = $auto_play ? "autoplay" : "";
+    
     # Might as well make this somewhat well-formed so that I can put
     # scripts in the document head section
     my $html = "<html><head>";
@@ -157,13 +176,11 @@ sub build_html {
 	$js_playlist=~s/,$//;
     }
     $html.="<script>\n";
-    $html.="var audio;\n";
-    $html.="var trackno   = 0;\n";
-    $html.="var playlist  = [ $js_playlist ];\n";
-    $html.="var len;\n";
-    $html.="var auto_play = $auto_play;\n";
-    $html.="var loop      = $loop;\n";
-    $html.="var delay     = $delay;\n";
+    $html.="var playlist     = [ $js_playlist ];\n";
+    $html.="var auto_play    = $auto_play;\n";
+    $html.="var auto_advance = $auto_advance;\n";
+    $html.="var loop         = $loop;\n";
+    $html.="var delay        = $delay;\n";
 
     # Now add our other script functions
     $html.=$player_script;    
@@ -196,11 +213,21 @@ sub build_widget {
     $self->set_gtk_vbox($vbox);
     $self->set_gtk_webkit_webview($wv);
 
+    # Normally you get "Not allowed to load local resource: ..." if
+    # you try to access a file URI without setting the base_uri to
+    # file:///something.... The base_uri method is preferable to
+    # turning this option on...
+    my $settings = Gtk2::WebKit::WebSettings->new;
+    $settings->set("enable-file-access-from-file-uris", 
+		   $self->get_allow_file_uri);
+    # $settings->set("enable-universal-access-from-file-uris",$allow_file_uri);
+    $wv->set_settings($settings);
+
     $self->build_html;
     
     $vbox->add($wv);
 
-    # Call super to tell it which Gtk widget we are
+    # Call super to tell it which Gtk widget to place
     $self->set_gtk_widget($vbox);
     
     1;
@@ -212,82 +239,3 @@ sub build_widget {
 
 __END__
     
-    my $quit = Gtk2::Button->new("Quit");
-    $quit->signal_connect ("clicked", sub { Gtk2->main_quit; });
-
-$vbox->add($quit);
-
-$mw->show_all;
-Gtk2->main;
-
-package UI::WebView;
-
-use Glib qw/TRUE FALSE/;
-use Gtk2::WebKit;
-
-# simple accessors
-sub gtk {    shift -> {gtk};  }
-
-sub new {
-    my $class = shift;
-    my $self  = {};
-
-    bless $self, $class;
-
-    my $wv = $self->{gtk} = Gtk2::WebKit::WebView->new;
-#    $wv->set_size(600,400);
-
-    # Ignore this part; jump straight to accessing local files without
-    # using custom callbacks
-    if (0) {
-	$wv->load_html_string(
-	    'Please click on <a href="custom://foo">me</a>',
-	    'file://');
-	$wv->signal_connect("resource-request-starting", 
-			    sub { $self->load_file_callback(@_) });
-    }
-
-    # OK, we get "Not allowed to load local resource: ..." for the below
-    #my $settings = $wv->get_settings;
-    my $settings = Gtk2::WebKit::WebSettings->new;
-    $settings->set("enable-file-access-from-file-uris",TRUE);
-    $settings->set("enable-universal-access-from-file-uris",TRUE);
-    $wv->set_settings($settings);
-
-    # $wv->hide; # doesn't seem to do anything
-
-    $wv->load_html_string(
-	'An image:<p><a href="core_6000/assets0/assets/legacy/images/02/5231321.jpg">
-           <img src="core_6000/assets0/assets/legacy/images/02/5231321.jpg">
-         </a>' . 
-	'<p> <audio autoplay id="audio" preload="auto" tabindex="0"  >
-          <source src="file:///home/dec/JW03182A.mp3"" type="audio/mp3">
-          Your browser does not support the audio element.
-         </audio> '
-,
-	'file:///home/dec/JLPT_Study/');
-
-    
-    return $self;
-}
-
-# failed attempt: ...
-sub load_file_callback {
-
-    my $self = shift or die;
-
-    my ($view, $frame, $resource, $net_req) = @_;
-    
-    warn "got here ($self)\n";
-    print "Got args:\n" . join "\n", (map { ref($_) } @_);
-
-    my $new_resource = Gtk2::WebKit::WebResource->new(
-	"New page", "custom://foo", "text/html", "UTF-8", $frame->get_name);
-
-    # crash with infinite loop
-    # $frame->load_uri("custom://foo");
-
-    #    $resource->set_data("data", "New Page");
-
-    return 1;
-}
--- uber/ff-audio-test.pl
+++ uber/ff-audio-test.pl
@@ -7,9 +7,11 @@ use Gtk2 qw/-init/;
 
 use Gtk2::Ex::FormFactory;
 
-require 'ff-audio.pm';
+# This is the new widget class
+use FormFactory::AudioPlayer;
 
 my $context = Gtk2::Ex::FormFactory::Context->new;
+my $ap_object;
 
 my $ff = Gtk2::Ex::FormFactory->new(
     context => $context,
@@ -20,12 +22,13 @@ my $ff = Gtk2::Ex::FormFactory->new(
 	    width  => 400,
 	    quit_on_close => 1,
 	    content => [
-		Gtk2::Ex::FormFactory::AudioPlayer->new(
-		    track_delay_ms => 100,
+		$ap_object = Gtk2::Ex::FormFactory::AudioPlayer->new(
+		    track_delay_ms => 600,
 		    auto_play      => 1,
+		    auto_advance   => 1,
 		    uri_base       => 'file:///home/dec/JLPT_Study/',
 		    playlist       => ['JW03182A.mp3','2lck24b1d1lv4.mp3'],
-		    allow_file_uri => 1,
+		    allow_file_uri => 0,
 		    loop => 1,
 		),
 	    ],
@@ -35,5 +38,7 @@ my $ff = Gtk2::Ex::FormFactory->new(
 
 $ff->open;
 $ff->update; 
+
+print "AudioPlayer object's name is " . $ap_object->get_name . "\n";
 
 Gtk2->main;
