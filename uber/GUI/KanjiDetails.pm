1d658e5068852318782b493e4dea0c90f60025ff
Declan Malone <idablack@users.sourceforge.net>
2016-10-06 22:14:16 +0100

    Start adding basic features for KanjiDetails window.

--- uber/GUI/KanjiDetails.pm
+++ uber/GUI/KanjiDetails.pm
@@ -7,24 +7,25 @@ use Model::KanjiReadings;
 
 
 our $format = <<'_END';
-+---------+-+----------------+-----------+---------+
-^ Image   | | RTKOfficial    | MyRTK     | TagHead |
-|         | +----------------+-----------+---------+
-|         | | RTKStory                   | TagList |
++->-------+-+->--------------+->---------+---------+
+^ Image   | ' RTKOfficial    ' MyRTK     ' TagHead |
+|         | |                |           |         |
+|         | +->--------------+-----------+---------+
+|         | ^ RTKStory                   ^ TagList |
 |         | |                            |         |
 +---------+ |                            +---------+
-^ JLPT    | |                            | NewTag  |
+' JLPT    | |                            ' NewTag  |
 +---------+ +----------------------------+---------+
-^ Jouyou  | | OtherEnglish                         |
+' Jouyou  | ' OtherEnglish                         |
 +---------+ +--------------------------------------+
-^ RTK     | | NoteHead                             |
-+---------+ +--------------------------------------+
-| Status  | | Notes                                |
+' RTK     | | NoteHead                             |
++---------+ +->------------------------------------+
+' Status  | ^ Notes                                |
 |         | |                                      |
 |         | |                                      |
 +---------+-+--------------------------------------+
 _END
-
+our $kanjivg_dir = '/home/dec/JLPT_Study/kanjivg/kanjivg-r20160426/kanji';
 
 sub new {
     my $class = shift;
@@ -36,6 +37,10 @@ sub new {
     );
 
     my $kanji   = $opts{kanji}   or die "KanjiWindow needs kanji => char option\n";
+
+    # Rethinking how FF works. Why not have a separate context for
+    # each KanjiDetails window? 
+    
     my $context = $opts{context} or die "KanjiWindow needs context => ref option\n";
 
     # We want a unique object name for attributes in this window
@@ -43,6 +48,7 @@ sub new {
 
     my $self = bless { context => $context, kanji => $kanji,
 		       toplevel => $opts{toplevel},
+		       basename => $basename,
 		       _kanji => KanjiReadings::Kanji->retrieve($kanji) };
 
 
@@ -63,7 +69,13 @@ sub build_window {
 
     my $self = shift;
     my $context = $self->{context};
+    my $basename = $self->{basename};
 
+    $context->add_object(
+	name => "$basename.notes",
+	object => $self
+    );
+    
     # I don't want every keystroke in a notes-type box to trigger a
     # database write, so I'll try setting sync to false on the
     # top-level FormFactory. That way I hope to be able to persist all
@@ -73,22 +85,47 @@ sub build_window {
 
     $self->{ff} = Gtk2::Ex::FormFactory->new(
 	sync    => 0,
+	context => $context,
 	content => [
 	    Gtk2::Ex::FormFactory::Window->new(
 		label => "Editing Kanji . $self->{kanji}",
-		context => $context,
 		content => [
 		    $self->build_table,
-		]
+		],
+		closed_hook => sub {
+		    $self->{ff}->ok();
+		}
 	    )
 	]
-    )
+    );
 }
 
+sub get_story {
+    "Default story value"
+}
+sub set_story {
+    my $self = shift;
+    warn "in get_notes";
+    warn "Setting story to $_[0]\n";
+    $self->{story} = $_[0];
+}
+
+sub get_notes {
+    my $self = shift;
+    warn "in get_notes";
+    $self->{notes};
+}
+sub set_notes {
+    my $self = shift;
+    warn "Setting notes to $_[0]\n";
+    $self->{notes} = $_[0];
+}
 
 sub build_table {
     my $self = shift;
     my $kanji = $self->{kanji};
+    my $basename = $self->{basename};
+    warn "Basename is $basename\n";
     Gtk2::Ex::FormFactory::Table->new(
 	expand => 1,
 	layout => $format,
@@ -96,8 +133,8 @@ sub build_table {
 	    $self->build_kanjivg_image,
 	    Gtk2::Ex::FormFactory::Label->new(label => "RTK Official"),
 	    Gtk2::Ex::FormFactory::Label->new(label => "My RTK"),
-	    Gtk2::Ex::FormFactory::Label->new(label => "TagHead"),
-	    Gtk2::Ex::FormFactory::Label->new(label => "RTK Story"),
+	    Gtk2::Ex::FormFactory::Label->new(label => "Tags"),
+	    Gtk2::Ex::FormFactory::TextView->new(attr => "$basename.story"),
 	    Gtk2::Ex::FormFactory::Label->new(label => "Tag List"),
 	    Gtk2::Ex::FormFactory::Label->new(label => "JLPT"),
 	    Gtk2::Ex::FormFactory::Label->new(label => "New Tag"),
@@ -106,19 +143,29 @@ sub build_table {
 	    Gtk2::Ex::FormFactory::Label->new(label => "RTK"),
 	    Gtk2::Ex::FormFactory::Label->new(label => "Enter kanji notes below"),
 	    Gtk2::Ex::FormFactory::Label->new(label => "Status"),
-	    Gtk2::Ex::FormFactory::Label->new(label => "Notes"),
+	    Gtk2::Ex::FormFactory::TextView->new(attr => "$basename.notes"),
 	]
     );
-    
+}
+
+sub 
+
+sub get_kanji_file {
+    my $self = shift;
+    my $kanji = $self->{kanji};
+    warn "Asked to get image file, kanji is $kanji\n";
+    my $unicode = sprintf("%05x", ord $kanji);
+
+    my $filename = "$kanjivg_dir/$unicode.svg";
+    return $filename;
 }
 
 sub build_kanjivg_image {
     my $self = shift;
     my $basename = $self->{basename};
-    Gtk2::Ex::FormFactory::KanjiVG->new(
-	kanji => $self->{kanji},
-	attr => "$basename.kanji",
+    Gtk2::Ex::FormFactory::Image->new(
+	attr => "$basename.kanji_file",
 	bgcolor => "#ffffff",
-	scale_to_fit => 1, 
+	scale_to_fit => 1,
     );
 }
